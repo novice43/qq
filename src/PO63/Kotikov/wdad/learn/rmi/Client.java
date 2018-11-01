3bbe0c77edb710449203759feb1c1412ea00e0b4
novice43 <bigwilly4307@gmail.com>
2018-11-02 01:57:14 +0400

    Hate DOM, XPath. JAXB love. For OOP and Neighborhood shoot in a upor

--- src/PO63/Kotikov/wdad/learn/rmi/Client.java
+++ src/PO63/Kotikov/wdad/learn/rmi/Client.java
@@ -38,8 +38,6 @@ public class Client
             rmiRegistry = LocateRegistry.getRegistry(preferencesManager.getProperty(PreferencesManagerConstants.REGISTRY_ADDRESS),
                     preferencesManager.getPort());
             remoteObject = (XmlDataManager)rmiRegistry.lookup(remoteObjectName);
-            //RegistryInfo.parse(PreferencesManager.getRmi(preferencesManager.getRootElement()).getServer().getRegistryOrBindedobject());
-            remoteObject = (XmlDataManager)rmiRegistry.lookup((RegistryInfo.registries.get(0).bindedObjects.get(0)).getName());
         }
         catch (Exception ex)
         {
